d088d03625e2f2e970afa9b878b0649e6f6cf240
Jonatandb <jonatandb@gmail.com>
2023-10-14 17:20:51 -0300

    Adding socket.io-client
    Connecting frontend to backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import http from 'http'
-import {Server as SocketServer } from 'socket.io';
+import { Server as SocketServer } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app)
-const io = new SocketServer(server)
+const io = new SocketServer(server, {
+  cors: {
+    origin: 'http://localhost:5173'
+  }
+})
+
+io.on('connection', socket => {
+  console.log('Client connected');
+})
 
 const PORT = 4000
 
